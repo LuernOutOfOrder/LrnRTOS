aebe4cb22b20cd9a90aab31a6d30a9c3699e5527
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-10-21 14:26:37 +0200

    fix: debug binary to work well

--- src/arch/riscv32/start.rs
+++ src/arch/riscv32/start.rs
@@ -1,19 +1,20 @@
-use core::arch::global_asm;
+use core::{arch::global_asm, fmt::Write};
 
-use crate::{devices::serials::set_kconsole, log::BootWriter};
+use crate::{devices::serials::set_kconsole, log::BootWriter, print::write_fmt};
 
 // Call entry point from asm
 global_asm!(
     "
-    .globl kstart
+    .section .text.entry
+    .global kstart
+    .type kstart, @function
     kstart:
-        la sp, stack_top
-        j _start
+        la sp, __stack_top    # load address of __stack_top
+        j _start              # jump to your Rust _start
     ",
 );
 
 #[unsafe(no_mangle)]
-#[unsafe(link_section = ".text.entry")]
 unsafe extern "C" fn _start(_hartid: usize, dtb: usize) -> ! {
     static mut EARLY_WRITER: BootWriter = BootWriter {
         base_addr: 0x1000_0000 as *mut u8,
