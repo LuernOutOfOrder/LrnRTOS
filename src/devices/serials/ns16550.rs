a755aa98f8f143e70b8bff4218d234c1c072cf57
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-11-04 11:53:52 +0100

    feat: improve error handling when init ns16550

--- src/devices/serials/ns16550.rs
+++ src/devices/serials/ns16550.rs
@@ -52,15 +52,17 @@ impl Ns16550 {
     /// Init a new Ns16550 from the given fdt node
     pub fn init(node: &FdtNode) {
         // Get address and size cells
-        let address_cells = get_node_prop_in_hierarchy(node, "#address-cells").unwrap();
-        let size_cells = get_node_prop_in_hierarchy(node, "#size-cells").unwrap();
+        let address_cells = get_node_prop_in_hierarchy(node, "#address-cells")
+            .expect("ERROR: ns16550 node is missing '#address-cells' property from parent bus\n");
+        let size_cells = get_node_prop_in_hierarchy(node, "#size-cells")
+            .expect("ERROR: ns16550 node is missing '#size-cells' property from parent bus\n");
         // Ptr read address and size cells value from off and cast it to u32 target's endianness
         let address_cells_val: u32 =
             u32::from_be(unsafe { ptr::read(address_cells.off_value as *const u32) });
         let size_cells_val: u32 =
             u32::from_be(unsafe { ptr::read(size_cells.off_value as *const u32) });
         // Get device memory region
-        let reg = get_node_prop(node, "reg").unwrap();
+        let reg = get_node_prop(node, "reg").expect("ERROR: ns16550 node is missing '#address-cells' property");
         let mut reg_buff: ArrayVec<u32, 120> = ArrayVec::new();
         let mut reg_cursor = reg.off_value;
         // Divide reg.value_len by 4 because we read u32 and not u8
