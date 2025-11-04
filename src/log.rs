cc1772376a5ad44ed8407eb2eeb1c570ff3c5b99
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-11-04 11:33:18 +0100

    docs: add missing comment on structure and function in basic module like log or print an remove unused bootwriter init in riscv32 entry point

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,10 @@
+/// Structure that is used for debugging purpose, used to print at a given address before devices
+/// are initialized
 pub struct BootWriter {
     pub base_addr: *mut u8,
 }
 
+/// Implement fmt::Write for BootWriter to allow format
 impl core::fmt::Write for BootWriter {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
         for b in s.bytes() {
