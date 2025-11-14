b5daf7d859f4981af6fa22d235be3c7bc70f5133
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-11-14 11:56:32 +0100

    feat(drivers): update riscv_cpu_intc driver to avoid using early boot struct and update clint0 to use cpu_intc new struct and ptr to this struct

--- src/devices/timer/clint0.rs
+++ src/devices/timer/clint0.rs
@@ -1,30 +1,27 @@
-use core::ptr;
+use core::ptr::{self, null_mut};
 
 use arrayvec::ArrayVec;
 
 use crate::{
-    devices::{DriverRegion, cpu_intc::riscv_cpu_intc::CpuIntc},
+    devices::{
+        DriverRegion,
+        cpu_intc::riscv_cpu_intc::{CPU_INTC_POOL, CpuIntc},
+    },
     dtb::{
         FdtNode,
-        helpers::{get_node_by_phandle, get_node_prop, get_node_prop_in_hierarchy},
+        helpers::{get_fdt_node, get_node_by_phandle, get_node_prop, get_node_prop_in_hierarchy},
     },
+    kprint,
 };
 
 /// Structure for sifive clint device driver
+#[derive(Copy, Clone, Debug)]
 pub struct Clint0 {
     region: DriverRegion,
     interrupt_extended: [Interrupt; 4],
 }
 
 #[derive(Copy, Clone, Debug)]
-struct EarlyBootInterrupt {
-    phandle: u32,
-    // Field to follow the len of the irq_ids array to avoid crushing valid data
-    irq_len: usize,
-    // Array of all irq
-    irq_ids: [u32; 4],
-}
-
 pub struct Interrupt {
     cpu_intc: *mut CpuIntc,
     // Field to follow the len of the irq_ids array to avoid crushing valid data
@@ -32,6 +29,15 @@ pub struct Interrupt {
     // Array of all irq
     irq_ids: [u32; 4],
 }
+
+pub static mut CLINT_POOL: [Clint0; 4] = [Clint0 {
+    region: DriverRegion { addr: 0, size: 0 },
+    interrupt_extended: [Interrupt {
+        cpu_intc: null_mut(),
+        irq_len: 0,
+        irq_ids: [0u32; 4],
+    }; 4],
+}; 4];
 
 impl Clint0 {
     pub fn init(node: &FdtNode) {
@@ -76,15 +82,16 @@ impl Clint0 {
                 size: device_size as usize,
             }
         }
-        let interrupt: EarlyBootInterrupt = EarlyBootInterrupt {
-            phandle: 0,
+        let interrupt: Interrupt = Interrupt {
+            cpu_intc: null_mut(),
             irq_len: 0,
             irq_ids: [0u32; 4],
         };
-        let mut interrupt_extended_array: [EarlyBootInterrupt; 4] = [interrupt; 4];
+        let mut intc_extended_array: [Interrupt; 4] = [interrupt; 4];
         let interrupt_extended = get_node_prop(node, "interrupts-extended")
             .expect("ERROR: clint0 node is missing 'interrupts-extended' property\n");
-        // First parsing through interrupts-extended to build complete array with values
+        // First parsing through interrupts-extended to build complete array with values from
+        // interrupts-extended property in fdt
         let mut interrupt_extended_cursor: usize;
         let mut interrupts_extended_vec: ArrayVec<u32, 16> = ArrayVec::new();
         {
@@ -98,7 +105,7 @@ impl Clint0 {
         }
         interrupt_extended_cursor = interrupt_extended.off_value;
         let mut iter_safety: usize = 0;
-        // Parse interrupts-extended props
+        // Second parsing through interrupts-extended to associate correct irqs with hart id
         for mut i in 0..interrupts_extended_vec.len() {
             let value = u32::from_be(unsafe { ptr::read(interrupt_extended_cursor as *const u32) });
             // Get node from interrupt-extended value
@@ -112,23 +119,28 @@ impl Clint0 {
                 .expect("ERROR: clint0 phandle node is missing the property '#interrupt-cells'");
             // Read node interrupt-cells value to know how many clint interrupt-extended value to
             // read and assign to phandle
+            let cpu_node = get_fdt_node(node.parent_node_index.unwrap());
+            let cpu_reg = get_node_prop(&cpu_node, "reg")
+                .expect("ERROR: failed to get core id from associated core from intc");
+            let cpu_reg_value = u32::from_be(unsafe { ptr::read(cpu_reg.off_value as *const u32) });
             let node_interrupt_cells_value =
                 u32::from_be(unsafe { ptr::read(node_interrupt_cells.off_value as *const u32) });
-            let mut parsed_interrupt: EarlyBootInterrupt = EarlyBootInterrupt {
-                phandle: value,
+            let mut cpu_intc_driver = unsafe { CPU_INTC_POOL[cpu_reg_value as usize] };
+            let mut parsed_interrupt: Interrupt = Interrupt {
+                cpu_intc: &mut cpu_intc_driver,
                 irq_len: 0,
                 irq_ids: [0u32; 4],
             };
-            // Check if an interrupt for this phandle already exist
+            // // Check if an interrupt for this phandle already exist
             #[allow(clippy::needless_range_loop)]
-            for e in 0..interrupt_extended_array.len() {
-                if interrupt_extended_array[e].phandle != value {
+            for e in 0..intc_extended_array.len() {
+                if intc_extended_array[e].cpu_intc != &mut cpu_intc_driver {
                     continue;
                 } else {
                     // Update current parsed interrupt with existing one
-                    parsed_interrupt = interrupt_extended_array[e];
+                    parsed_interrupt = intc_extended_array[e];
                     // Update i iterator to be the same index as e to retrieve it in
-                    // 'interrupt_extended_array'
+                    // 'intc_extended_array'
                     i = e;
                 }
             }
@@ -146,15 +158,12 @@ impl Clint0 {
             // Increment iterator
             iter_safety += 1;
             // Update array with current interrupt
-            interrupt_extended_array[i] = parsed_interrupt;
+            intc_extended_array[i] = parsed_interrupt;
         }
-        // TODO: create second interrupt_extended_array but for after early boot
-        // Copy content of interrupt_extended_array to final interrupt array and create struct with
-        // ptr to cpu_intc driver
-
-        // let clint0: Clint0 = Clint0 {
-        //     region: device_addr,
-        //     interrupt_extended: interrupt_extended_array,
-        // };
+        let clint0: Clint0 = Clint0 {
+            region: device_addr,
+            interrupt_extended: intc_extended_array,
+        };
+        kprint!("debug: {:?}\n", intc_extended_array);
     }
 }
