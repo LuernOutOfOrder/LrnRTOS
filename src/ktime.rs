6ace78a2f865062095803294812438f445024093
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-11-17 16:32:36 +0100

    feat: add ktime mod to get time from clint0 drivers for now and convert to correct value(ms, ns and seconds)

--- src/ktime.rs
+++ src/ktime.rs
@@ -23,3 +23,13 @@ pub fn ktime_ns() -> u64 {
     let mtime = unsafe { CLINT_DEVICE.read_mtime() };
     (mtime * 1_000_000) / cpu_freq as u64
 }
+
+pub fn set_ktime_ms(delay: u64) {
+    #[allow(static_mut_refs)]
+    let mtime = unsafe { CLINT_DEVICE.read_mtime() };
+    let delta_mtime = mtime + delay;
+    #[allow(static_mut_refs)]
+    unsafe {
+        CLINT_DEVICE.set_mtimecmp(0, delta_mtime)
+    };
+}
