85a240ad0be4cae4ba92ef3cec755c11d59bd864
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-11-25 09:23:47 +0100

    feat(ktime): move ktime.rs file to ktime mod for future update

--- src/ktime/mod.rs
+++ src/ktime/mod.rs
@@ -1,4 +1,4 @@
-use crate::devices::{cpufreq::CPUFREQ, timer::clint0::CLINT_DEVICE};
+use crate::drivers::{cpufreq::CPUFREQ, timer::clint0::CLINT_DEVICE};
 
 pub fn ktime_seconds() -> u64 {
     #[allow(static_mut_refs)]
@@ -24,22 +24,4 @@ pub fn ktime_ns() -> u64 {
     (mtime * 1_000_000) / cpu_freq as u64
 }
 
-pub fn set_ktime_ms(delay: u64) {
-    #[allow(static_mut_refs)]
-    let mtime = unsafe { CLINT_DEVICE.read_mtime() };
-    let delta_mtime = mtime + delay;
-    #[allow(static_mut_refs)]
-    unsafe {
-        CLINT_DEVICE.set_mtimecmp(0, delta_mtime)
-    };
-}
-
-pub fn set_ktime_tick_safety(tick: u64) {
-    #[allow(static_mut_refs)]
-    let mtime = unsafe { CLINT_DEVICE.read_mtime() };
-    let delta_mtime = mtime + tick;
-    #[allow(static_mut_refs)]
-    unsafe {
-        CLINT_DEVICE.set_mtimecmp(0, delta_mtime)
-    };
-}
+pub fn set_ktime_ms(_time: u64) {}
