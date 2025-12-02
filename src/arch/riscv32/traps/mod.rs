13e3cf0f63b62294300d3ee90c4098825d206093
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-12-02 16:58:19 +0100

    feat(riscv32): add disable_interrupt fn

--- src/arch/riscv32/traps/mod.rs
+++ src/arch/riscv32/traps/mod.rs
@@ -1,6 +1,6 @@
 use interrupt::{
-    enable_mie_msie, enable_mie_mtie, enable_mstatus_mie, mscratch_set_trap_frame,
-    mtvec_set_trap_entry, mtvec_switch_to_direct_mode,
+    disable_mie_msie, disable_mie_mtie, disable_mstatus_mie, enable_mie_msie, enable_mie_mtie,
+    enable_mstatus_mie, mscratch_set_trap_frame, mtvec_set_trap_entry, mtvec_switch_to_direct_mode,
 };
 
 pub mod handler;
@@ -19,4 +19,10 @@ pub fn enable_interrupts() {
     enable_mie_msie();
     // Enable interrupt handling in exception handler
     enable_mstatus_mie();
+}
+
+pub fn disable_interrupts() {
+    disable_mie_mtie();
+    disable_mie_msie();
+    disable_mstatus_mie();
 }
