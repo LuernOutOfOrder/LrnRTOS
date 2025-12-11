65bccbe31e055c30f60a4fd3a0342bc4fe10b902
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-12-11 15:13:43 +0100

    refactor: remove generic devices struct from devices_info to devices/mod.r

--- src/devices_info.rs
+++ src/devices_info.rs
@@ -1,46 +1,4 @@
-use crate::drivers::DriverRegion;
-
-#[derive(Copy, Clone)]
-pub enum DeviceType {
-    Serial,
-    Timer,
-}
-
-pub trait DeviceInfo {}
-
-/// Structure used to define a serial device.
-/// Only used in static SERIAL_DEVICES
-#[derive(Copy, Clone)]
-pub struct DevicesHeader<'a> {
-    pub device_type: DeviceType,
-    pub compatible: &'a str,
-    pub device_addr: DriverRegion,
-}
-
-#[derive(Copy, Clone)]
-pub struct Devices<'a> {
-    pub header: DevicesHeader<'a>,
-    pub info: Option<*const dyn DeviceInfo>,
-}
-
-impl Devices<'_> {
-    pub const fn init() -> Self {
-        Devices {
-            header: DevicesHeader {
-                device_type: DeviceType::Serial,
-                compatible: "",
-                device_addr: DriverRegion { addr: 0, size: 0 },
-            },
-            info: None,
-        }
-    }
-}
-
-unsafe impl<'a> Sync for Devices<'a> {}
-
-pub struct SerialDevice {}
-
-impl DeviceInfo for SerialDevice {}
+use crate::{devices::{DeviceInfo, DeviceType, Devices, DevicesHeader, SerialDevice}, drivers::DriverRegion};
 
 static mut SERIAL_DEVICE: SerialDevice = SerialDevice {};
 
