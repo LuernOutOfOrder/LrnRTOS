14b5ec9a1565c2b5653b3a50060d34af478daee8
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-12-11 15:13:10 +0100

    feat: impl Devices to use basic init and add Timer generic struct

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,8 +1,63 @@
 use crate::{
-    devices_info::{DEVICES, DeviceType, Devices, DevicesHeader},
-    drivers::DriverRegion,
-    fdt::{FdtNode, fdt_present, helpers::fdt_get_node_by_compatible, parse_dtb_file},
+    devices_info::DEVICES, drivers::DriverRegion, fdt::{fdt_present, helpers::fdt_get_node_by_compatible, parse_dtb_file, FdtNode}
 };
+
+#[derive(Copy, Clone)]
+pub enum DeviceType {
+    Serial,
+    Timer,
+}
+
+pub trait DeviceInfo {}
+
+/// Structure used to define a serial device.
+/// Only used in static SERIAL_DEVICES
+#[derive(Copy, Clone)]
+pub struct DevicesHeader<'a> {
+    pub device_type: DeviceType,
+    pub compatible: &'a str,
+    pub device_addr: DriverRegion,
+}
+
+#[derive(Copy, Clone)]
+pub struct Devices<'a> {
+    pub header: DevicesHeader<'a>,
+    pub info: Option<*const dyn DeviceInfo>,
+}
+
+impl Devices<'_> {
+    pub const fn init() -> Self {
+        Devices {
+            header: DevicesHeader {
+                device_type: DeviceType::Serial,
+                compatible: "",
+                device_addr: DriverRegion { addr: 0, size: 0 },
+            },
+            info: None,
+        }
+    }
+}
+
+unsafe impl<'a> Sync for Devices<'a> {}
+
+pub struct SerialDevice {}
+
+pub struct TimerDevice {
+    interrupt_extended: [TimerDeviceInterrupt; 4],
+}
+
+pub struct TimerDeviceInterrupt {
+    // Ptr to CpuIntc struct
+    cpu_intc: usize,
+    // Field to follow the len of the irq_ids array to avoid crushing valid data
+    irq_len: usize,
+    // Array of all irq
+    irq_ids: [u32; 4],
+}
+
+// Implement DeviceInfo trait to all Device type structure
+impl DeviceInfo for SerialDevice {}
+impl DeviceInfo for TimerDevice {}
 
 // Boolean to define the type of info from devices to get.
 // true == FDT
