4333a44e11bd04dabf754f3ea1b2c5532a62fa94
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-12-11 16:45:50 +0100

    feat: add testing struct for fat ptr and it's not working yet, error on unwrap

--- src/drivers/timer/clint0.rs
+++ src/drivers/timer/clint0.rs
@@ -5,14 +5,15 @@ use core::ptr::{self};
 use arrayvec::ArrayVec;
 
 use crate::{
-    drivers::
-        DriverRegion
-    ,
+    devices::{DeviceType, InterruptExtended, TimerDevice, devices_get_info},
+    drivers::DriverRegion,
     fdt::{
+        FdtNode,
         helpers::{
             fdt_get_node, fdt_get_node_by_compatible, fdt_get_node_by_phandle, fdt_get_node_prop,
-        }, FdtNode
-    }, kprint_fmt,
+        },
+    },
+    kprint_fmt,
 };
 
 use super::{TIMER_SUBSYSTEM, Timer, TimerType};
@@ -21,7 +22,7 @@ use super::{TIMER_SUBSYSTEM, Timer, TimerType};
 pub struct Clint0 {
     region: DriverRegion,
     #[allow(unused)]
-    interrupt_extended: [Interrupt; 4],
+    interrupt_extended: [InterruptExtended; 4],
     timer_type: TimerType,
 }
 
@@ -51,7 +52,7 @@ pub struct Interrupt {
 
 static mut CLINT0_INSTANCE: Clint0 = Clint0 {
     region: DriverRegion { addr: 0, size: 0 },
-    interrupt_extended: [Interrupt {
+    interrupt_extended: [InterruptExtended {
         cpu_intc: 0,
         irq_len: 0,
         irq_ids: [0u32; 4],
@@ -59,97 +60,33 @@ static mut CLINT0_INSTANCE: Clint0 = Clint0 {
     timer_type: TimerType::ArchitecturalTimer,
 };
 
+#[repr(C)]
+struct RawTraitObject {
+    data: *const (),
+    vtable: *const (),
+}
+
 impl Clint0 {
     pub fn init() {
-        let node: &FdtNode = match fdt_get_node_by_compatible("sifive,clint0") {
-            Some(n) => n,
+        let device_info = match devices_get_info("sifive,clint0", DeviceType::Timer) {
+            Some(d) => d,
             None => return,
         };
-        let device_addr: DriverRegion = DriverRegion::new(node);
-        let interrupt: Interrupt = Interrupt {
-            cpu_intc: 0,
-            irq_len: 0,
-            irq_ids: [0u32; 4],
-        };
-        let mut intc_extended_array: [Interrupt; 4] = [interrupt; 4];
-        let interrupt_extended = fdt_get_node_prop(node, "interrupts-extended")
-            .expect("ERROR: clint0 node is missing 'interrupts-extended' property\n");
-        // First parsing through interrupts-extended to build complete array with values from
-        // interrupts-extended property in fdt
-        let mut interrupt_extended_cursor: usize;
-        let mut interrupts_extended_vec: ArrayVec<u32, 16> = ArrayVec::new();
-        {
-            interrupt_extended_cursor = interrupt_extended.off_value;
-            for _ in 0..interrupt_extended.value_len / 4 {
-                let value =
-                    u32::from_be(unsafe { ptr::read(interrupt_extended_cursor as *const u32) });
-                interrupts_extended_vec.push(value);
-                interrupt_extended_cursor += 4;
-            }
-        }
-        interrupt_extended_cursor = interrupt_extended.off_value;
-        let mut iter_safety: usize = 0;
-        // Second parsing through interrupts-extended to associate correct irqs with hart id
-        for mut i in 0..interrupts_extended_vec.len() {
-            let value = u32::from_be(unsafe { ptr::read(interrupt_extended_cursor as *const u32) });
-            // Get node from interrupt-extended value
-            if iter_safety == interrupts_extended_vec.len() {
-                break;
-            }
-            let node = fdt_get_node_by_phandle(value).expect(
-                "ERROR: cannot find associate phandle node from clint0 interrupts-extended property",
-            );
-            let node_interrupt_cells = fdt_get_node_prop(&node, "#interrupt-cells")
-                .expect("ERROR: clint0 phandle node is missing the property '#interrupt-cells'");
-            // Read node interrupt-cells value to know how many clint interrupt-extended value to
-            // read and assign to phandle
-            let cpu_node = fdt_get_node(node.parent_node_index.unwrap());
-            let cpu_reg = fdt_get_node_prop(&cpu_node, "reg")
-                .expect("ERROR: failed to get core id from associated core from intc");
-            let cpu_reg_value = u32::from_be(unsafe { ptr::read(cpu_reg.off_value as *const u32) });
-            let node_interrupt_cells_value =
-                u32::from_be(unsafe { ptr::read(node_interrupt_cells.off_value as *const u32) });
-            let mut parsed_interrupt: Interrupt = Interrupt {
-                cpu_intc: cpu_reg_value,
-                irq_len: 0,
-                irq_ids: [0u32; 4],
-            };
-            // // Check if an interrupt for this phandle already exist
-            #[allow(clippy::needless_range_loop)]
-            for e in 0..intc_extended_array.len() {
-                if intc_extended_array[e].cpu_intc != cpu_reg_value {
-                    continue;
-                } else {
-                    // Update current parsed interrupt with existing one
-                    parsed_interrupt = intc_extended_array[e];
-                    // Update i iterator to be the same index as e to retrieve it in
-                    // 'intc_extended_array'
-                    i = e;
-                }
-            }
-            // Push irqs inside 'irq_ids' array of current 'parsed_interrupt'
-            for _ in 0..node_interrupt_cells_value {
-                interrupt_extended_cursor += 4;
-                iter_safety += 1;
-                let irq_value =
-                    u32::from_be(unsafe { ptr::read(interrupt_extended_cursor as *const u32) });
-                parsed_interrupt.irq_ids[parsed_interrupt.irq_len] = irq_value;
-                parsed_interrupt.irq_len += 1;
-            }
-            // Increment offset
-            interrupt_extended_cursor += 4;
-            // Increment iterator
-            iter_safety += 1;
-            // Update array with current interrupt
-            intc_extended_array[i] = parsed_interrupt;
-        }
+        // Get struct behind trait
+        let device_info_trait = device_info.info.unwrap();
+        let raw: RawTraitObject = unsafe { core::mem::transmute(device_info_trait) };
+        let timer_device_ptr = raw.data as *const TimerDevice;
+        let timer_device_ref = unsafe { &*timer_device_ptr };
         // Init Clint0 driver and update timer sub-system for global access.
         let clint0: Clint0 = Clint0 {
-            region: device_addr,
-            interrupt_extended: intc_extended_array,
+            region: device_info.header.device_addr,
+            interrupt_extended: timer_device_ref.interrupt_extended,
             timer_type: TimerType::ArchitecturalTimer,
         };
-        kprint_fmt!("debug interrupt_extended: {:?}\n", clint0.interrupt_extended);
+        kprint_fmt!(
+            "debug interrupt_extended: {:?}\n",
+            clint0.interrupt_extended
+        );
         unsafe { CLINT0_INSTANCE = clint0 };
         // Allow static mut refs because it's only used on driver init, so no data race or UB
         // possible
